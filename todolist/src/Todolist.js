972dac45be3c012c18ee6901e0d68c52a4819d34
XIJIANGYUE233 <292302357@qq.com>
2022-09-06 18:48:29 +0800

    React学习-上传代码

---
+++ todolist/src/Todolist.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+ 
+
+function Todolist(){
+  return(
+    <Fragment>
+     <div> <input></input> <button>提交</button></div>
+      <ul>
+        <li>学英语</li>
+      </ul>
+    </Fragment>
+  );
+}
+export default Todolist;
+ 
